5b5cf7b85dfbea9f02358913dacb36d682c993b2
Sonia <zolcristal12@gmail.com>
2023-06-30 17:58:55 -0300

    Clase11#41 Agrego a conexion.py conectar y cerrar conexión y creo archivo pool_de_datos

---
+++ python/Clase_11/conexion.py
@@ -0,0 +1,64 @@
+from psycopg2 import pool
+from logger_base import log
+import sys
+
+
+# psycopg2 as bd # otra manera de importar psycopg2
+class Conexion:
+    _DATABASE = 'test_bd'
+    _USERNAME = 'postgres'
+    _PASSWORD = 'admin'
+    _BD_PORT = '5432'
+    _HOST = '127.0.0.1'
+    _MIN_CON = 1
+    _MAX_COM = 5
+    _pool = None
+
+    @classmethod
+    def obtenerConexion(cls):
+        conexion = cls.obtenerPool().getconn()
+        log.debug(f'Conexion obtenida del pool: {conexion}')
+        return conexion
+
+    @classmethod
+    def obtenerCursor(cls):
+       pass
+
+    @classmethod
+    def obtenerPool(cls):
+        if cls._pool is None:
+            try:
+                cls._pool = pool.SimpleConnectionPool(cls._MIN_CON,
+                                                      cls._MAX_COM,
+                                                      host = cls._HOST,
+                                                      user = cls._USERNAME,
+                                                      password = cls._PASSWORD,
+                                                      port = cls._BD_PORT,
+                                                      database = cls._DATABASE)
+                log.debug(f'Creacion del pool exitosa: {cls._pool}')
+                return  cls._pool
+            except Exception as e:
+                log.error(f'Ocurrio un error al obtener el pool: {e}')
+                sys.exit()
+        else:
+            return cls._pool
+
+@classmethod
+    def liberarConenxion(cls, conexion):
+        cls.obtenerPool().putconn(conexion)
+        log.debug(f´Regresamos
+        la
+        conexión
+        del pool: {conexion}´)
+
+        @classmethod
+        def cerrarConexiones(cls):
+            cls.obtenerPool().closeall()
+
+
+if __name__ == '__main__':
+    conexion1 = Conexion.obtenerConexion()
+    conexion2 = Conexion.obtenerConexion()
+    conexion3 = Conexion.obtenerConexion()
+    conexion4 = Conexion.obtenerConexion()
+    conexion5 = Conexion.obtenerConexion()
